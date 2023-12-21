33700dd01cd9b3fdf49e2f67193e27be37eb8696
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2023-12-21 19:04:55 +0800

    Add get_git_files() to return list of files

--- src/filter_git_files.py
+++ src/filter_git_files.py
@@ -3,9 +3,21 @@
 import subprocess
 
 
+def get_git_files() -> list[str]:
+    """Run git ls-files & return list of files.
+
+    Returns:
+        List of files.
+    """
+    p = subprocess.run(
+        ["git", "ls-files"], capture_output=True, check=True, text=True
+    )
+    return p.stdout.split("\n")
+
+
 def main() -> None:
     """Main function."""
-    p = subprocess.run(["git", "ls-files"], capture_output=True, check=True)
+    files = get_git_files()
 
 
 if __name__ == "__main__":
